228527a65636e7bc9b1ede16f82e3c84e69d9842
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-01-25 20:27:57 +0530

    Function

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -95,3 +95,32 @@
 
 
 
+
+
+
+// Select input element
+const titleEl = document.getElementById('title');
+
+// select list element
+const listEl = document.getElementById('list');
+
+// select button element
+const addButtonEl = document.getElementById('add-button');
+
+// adding "click" event listener on button element
+addButtonEl.addEventListener('click', function() {
+
+  // getting the value of the input field
+  const titleText = titleEl.value;
+  
+  // creating and 'li' element and adding inner text
+  const li = document.createElement('li');
+  li.innerText = titleText;
+
+  // add the newly created li element to the list
+  listEl.appendChild(li);
+
+  // clear the input field after adding li to the list
+  titleEl.value = '';
+
+})
