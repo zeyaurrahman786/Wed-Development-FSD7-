73bc9a28bc725e6acf23c45e34a30683eec56b2b
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-02-20 20:21:31 +0530

    Movie API

---
+++ API/Movie/script.js
@@ -0,0 +1,23 @@
+let button = document.querySelector('button')
+let input = document.querySelector('input')
+let div = document.querySelector('#list')
+
+button.addEventListener('click',()=>{
+    // console.log('Hello');
+    let searchText = input.value;
+    fetch (`https://api.tvmaze.com/search/shows?q=${searchText}`)
+    .then((data)=>{
+        return data.json();
+    })
+    .then((res)=>{
+        console.log(res[0].show.image.original);
+        show(res);
+    })
+})
+
+function show(data){
+    console.log(data[0].show.image.original,'Hiiii');
+    let img = document.createElement('img');
+    img.setAttribute('src',data[0].show.image.original)
+    div.append(img);
+}
