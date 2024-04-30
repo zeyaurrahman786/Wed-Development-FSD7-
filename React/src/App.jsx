1382199344bffe602d30f08e96504f89e15ec468
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-30 18:21:37 +0530

    useReducer

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -47,45 +47,81 @@
 
 
 
-import React, { useState } from 'react'
-import { useEffect } from 'react';
+// import React, { useState } from 'react'
+// import { useEffect } from 'react';
  
+
+// const App = () => {
+// let [data,setData]= useState([])
+
+// useEffect(()=>{
+//   fetch('https://dummyjson.com/recipes').then(response=>{
+
+//   return response.json()
+//   }).then(h => {
+//     // Code to handle the data received
+//     // console.log(h,"hhhhhhhhhhhhhhhhhhhhhh");
+//     setData(h.recipes)
+//   })
+//   .catch(error => {
+//     // Code to handle errors
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
+// },[])
+ 
+// console.log(data);
+
+
+//   return (
+//     <>{
+//     data.map(id=>{
+//       return (
+//         <div key={id}>
+//           <h1>{id.id}</h1>
+//           <h2>{id.name}</h2>
+          
+//         </div>
+//       )
+//     })
+//   }
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+import React from 'react'
+import { useReducer } from 'react';
+
+let reducer = (state, action) => {
+  if(action.type === "incr"){
+    return state + 1;
+  }
+  else if(action.type === "decr"){
+    return state - 1;
+  }
+  return state;
+}
 
 const App = () => {
-let [data,setData]= useState([])
 
-useEffect(()=>{
-  fetch('https://dummyjson.com/recipes').then(response=>{
-
-  return response.json()
-  }).then(h => {
-    // Code to handle the data received
-    // console.log(h,"hhhhhhhhhhhhhhhhhhhhhh");
-    setData(h.recipes)
-  })
-  .catch(error => {
-    // Code to handle errors
-    console.error('There was a problem with the fetch operation:', error);
-  });
-},[])
- 
-console.log(data);
-
-
+  let [state, dispatch] = useReducer(reducer,0)
+  
   return (
-    <>{
-    data.map(id=>{
-      return (
-        <div key={id}>
-          <h1>{id.id}</h1>
-          <h2>{id.name}</h2>
-          
-        </div>
-      )
-    })
-  }
-    </>
+    <div>
+      <p>{state}</p>
+
+      <button onClick={() => dispatch ({type : "incr"})}>Increment</button>
+      <button onClick={() => dispatch ({type : "decr"})}>Decrement</button>
+    </div>
   )
 }
 
-export default App
+export default App;
